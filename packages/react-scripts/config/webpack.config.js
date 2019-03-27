6be411ebbd95a17f7c10dd7b068b9efad2773cc0
bitttttten <bitttttten@googlemail.com>
2019-03-27 15:37:27 +0100

    add loadable plugin to client build

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -34,6 +34,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const eslint = require('eslint');
+const LoadablePlugin = require('@loadable/webpack-plugin');
 // @remove-on-eject-begin
 const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
 // @remove-on-eject-end
@@ -583,6 +584,8 @@ module.exports = function(webpackEnv) {
       ],
     },
     plugins: [
+      // https://www.smooth-code.com/open-source/loadable-components/docs/server-side-rendering/
+      new LoadablePlugin(),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
