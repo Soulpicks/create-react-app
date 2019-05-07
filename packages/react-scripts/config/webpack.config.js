d5a899cccafc1b845318e39a3974eed08c5e97a5
bitttttten <bitttttten@googlemail.com>
2019-05-07 15:00:54 +0200

    remove webp setting

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -427,10 +427,6 @@ module.exports = function(webpackEnv) {
                       quality: '65-90',
                       speed: 4,
                     },
-                    // the webp option will enable WEBP
-                    webp: {
-                      quality: 75,
-                    },
                   },
                 },
               ],
